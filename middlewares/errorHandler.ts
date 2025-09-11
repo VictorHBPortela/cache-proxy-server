d81302dcde74c0cf5cc1883b6fdf5187d29f9815
VictorHBPortela <victor.portela@duotalk.com.br>
2025-09-11 20:18:34 -0300

    refactor: clean up ESLint rules and improve code formatting in middleware and response utilities

--- middlewares/errorHandler.ts
+++ middlewares/errorHandler.ts
@@ -1,12 +1,8 @@
-import type { Request, Response, NextFunction } from 'express';
-import { errorResponse } from '../utils/responses.js';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { Request, Response } from "express";
+import { errorResponse } from "../utils/responses.js";
 
-export function errorHandler(
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export function errorHandler(err: any, req: Request, res: Response): void {
   console.error(err);
   errorResponse(res, 500, err);
 }
--- utils/responses.ts
+++ utils/responses.ts
@@ -1,9 +1,18 @@
-import type { Response } from 'express';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { Response } from "express";
 
-export function successResponse(res: Response, data: any, message = 'Success') {
-  return res.status(200).json({ success: 'true', message, data });
+export function successResponse(
+  res: Response,
+  data: any,
+  message = "Success"
+): Response {
+  return res.status(200).json({ success: "true", message, data });
 }
 
-export function errorResponse(res: Response, status: number, error: string) {
-  return res.status(status).json({ success: 'false', error });
+export function errorResponse(
+  res: Response,
+  status: number,
+  error: string
+): Response {
+  return res.status(status).json({ success: "false", error });
 }
